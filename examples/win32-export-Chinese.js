020c8804cb0038a561799e1b801d2aa563cbe6cf
carsonDB <2278891315@qq.com>
2021-11-16 15:21:45 +0800

    fix ">" in text cause bug

---
+++ examples/win32-export-Chinese.js
@@ -0,0 +1,13 @@
+const say = require('../')
+const path = require('path')
+
+// Export spoken audio to a WAV file
+say.setEncoding('gbk')
+const destFile = 'tmp-test.wav'
+say.export("您好>吗", undefined, 1, path.join(__dirname, destFile), (err) => {
+  if (err) {
+    return console.error(err)
+  }
+
+  console.log(`Text has been saved to ${destFile}.`)
+})
